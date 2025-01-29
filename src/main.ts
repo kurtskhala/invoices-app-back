10a09fc4a3d0a06147c4277826795d2ba649f10a
kurtskhala <92853046+kurtskhala@users.noreply.github.com>
2025-01-29 20:14:31 +0400

    cors problem

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://shark-app-jk6jb.ondigitalocean.app/users',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
