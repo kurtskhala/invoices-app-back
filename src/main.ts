1779b005c7a120470799a5ce2a0629c744aed9d1
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2025-01-29 20:39:39 +0400

    added right origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://invoices-jeckxjgw0-kurtskhalas-projects.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
