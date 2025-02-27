1254dd93c3886ffd3c24c734e438ec7beb50695c
Nino Kharazishvili <ninokharazishvili@credo.ge>
2025-02-27 11:18:08 +0400

    added cors for my local

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://invoices-app-seven.vercel.app',
+    origin: ['https://invoices-app-seven.vercel.app', 'http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
